5ad4d19aef96787e9ac8730f423c945bebe7c52c
kellenThaisaLima <kellen1393@gmail.com>
2022-05-26 10:26:07 -0300

    Corrigido classe teste

--- src/atividadeWebConfFaltantes/TstLocacao.java
+++ src/atividadeWebConfFaltantes/TstLocacao.java
@@ -4,13 +4,18 @@ public class TstLocacao {
 
 	public static void main(String[] args) {
 		
+		Cidade cid2 = new Cidade();
+		cid2.setCodCid(2);
+		cid2.setNomeCid("Araucaria");
+		
+		Endereco endereco2 = new Endereco();		
+		endereco2.setRua("Rua das Flores");
+		endereco2.setNum(2);
+		endereco2.setCidade(cid2);
+		
 		Locacao loc = new Locacao();
-		loc.getDataInicio();
-		loc.getDataFim();
-		loc.getImovel();
-		loc.getImovel().getEndereco();
-		loc.getImovel().getValAluguel();
-		loc.getImovel().getTipoMaterial();		
+		loc.getImovel().setEndereco(endereco2);
+	
 		
 		
 		loc.toString();
