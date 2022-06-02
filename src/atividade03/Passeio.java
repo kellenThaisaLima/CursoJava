adfbcbae01ab31e4fdbbe063ac81aadccf27946f
kellenThaisaLima <kellen1393@gmail.com>
2022-06-02 12:28:18 -0300

    Incluido Classes Carga e Passeio, alterado a classe teste, testando relacionamento, herança, polimorfismo

---
+++ src/atividade03/Passeio.java
@@ -0,0 +1,54 @@
+package atividade03;
+
+final public class Passeio extends Veiculo{
+	
+	private int qtdPassageiros;
+	
+	public Passeio() {
+		
+		this.qtdPassageiros = 0;
+	};
+	
+	public Passeio(int qtdPassageiros) {
+		
+		this.qtdPassageiros = qtdPassageiros;
+	};
+	
+
+	public int getQtdPassageiros() {
+		return qtdPassageiros;
+	}
+
+	final public void setQtdPassageiros(int qtdPassageiros) {
+		this.qtdPassageiros = qtdPassageiros;
+	}
+
+	@Override
+	float calcVel(float velocMax) {
+		float parseMetros = getVelocMax()/1000;
+		return parseMetros;
+	}
+
+	@Override
+	public String toString() {
+		super.setPlaca("55555");
+		super.setMarca("Fiat");
+		super.setModelo("Uno");
+		super.setCor("Vermelho");		
+		super.setQtdRodas(4);
+		super.getMotor().setQtdPist(4);
+		super.getMotor().setPotencia(70);
+		System.out.println("Veículo de Passeio: " + hashCode());
+        System.out.println("QtdPassageiros: " + this.qtdPassageiros);      
+        System.out.println("A velocidade do veículo de carga é " + this.calcVel(getVelocMax()) + " m/h"); 
+        System.out.println("************************************************************");        
+        return " Veículo de Passeio: " + hashCode();
+	}
+	
+	
+	
+	
+	
+	
+
+}
